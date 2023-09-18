705d4519ac89647d074f2834e83c9a697c911287
Indra Gunawan <ingunawandra@gmail.com>
2023-09-18 17:27:24 +0000

    fix(migration): wrong file name

--- databases/migrations/000001_create_users_table.down.sql
+++ databases/migrations/000001_create_users_table.down.sql
@@ -1,8 +1,1 @@
-CREATE TABLE IF NOT EXISTS "users"(
-   id serial PRIMARY KEY,
-   username VARCHAR (255) UNIQUE NOT NULL,
-   password VARCHAR (255) NOT NULL,
-   email VARCHAR (255) UNIQUE NOT NULL,
-   created_at TIMESTAMP NOT NULL,
-   updated_at TIMESTAMP
-);
+DROP TABLE IF EXISTS "users";
